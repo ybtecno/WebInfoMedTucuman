6001b085ba7b02b9f7363b0103ca597ee6a36c5a
Fede <fpesao@gmail.com>
2018-12-11 17:36:32 -0300

    SW
    Se carga en main

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+//Service Worker
+
+if('serviceWorker' in navigator){
+  console.log('Puedes usar los serviceWorker en tu navegador');
+
+  navigator.serviceWorker.register('./sw.js')
+                        .then(res => console.log('serviceWorker cargado correctamente', res))
+                        .catch(err => console.log('serviceWorker no se ha podido registrar', err));
+                        
+
+}else{
+  console.log('NO PUEDES usar los serviceWorker en tu navegador');
+}
+
 jQuery(document).ready(function($) {
 
   // Header fixed and Back to top button
