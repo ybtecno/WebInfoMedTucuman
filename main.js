9e603abecd66cc1d8499e530b23adc2dea6ed1ba
Fede <fpesao@gmail.com>
2018-12-17 13:06:50 -0300

    Update CartillaMedica

--- main.js
+++ main.js
@@ -191,4 +191,17 @@ $(function() {
       $('#segundos').html(event.strftime('%S'));
   });
 
+  // Porfolio isotope and filter
+  var portfolioIsotope = $('.portfolio-container').isotope({
+    itemSelector: '.portfolio-item',
+    layoutMode: 'fitRows'
+  });
+
+  $('#portfolio-flters li').on( 'click', function() {
+    $("#portfolio-flters li").removeClass('filter-active');
+    $(this).addClass('filter-active');
+
+    portfolioIsotope.isotope({ filter: $(this).data('filter') });
+  });
+
 });
